e2dbbeb281d4fc993321fd5bf31023e7996bc712
baransahinn <sahinbaran611@gmail.com>
2024-01-13 01:13:16 +0300

    güncelleme

--- js14/app.js
+++ js14/app.js
@@ -1,19 +1,35 @@
 // OOP
-class Insan {
-  constructor(isim, soyisim, yas, maas) {
-    this.isim = isim;
-    this.soyisim = soyisim;
-    this.yas = yas;
-    this.maas = maas;
-  }
+// class Insan {
+//   constructor(isim, soyisim, yas, maas) {
+//     this.isim = isim;
+//     this.soyisim = soyisim;
+//     this.yas = yas;
+//     this.maas = maas;
+//   }
 
-  bilgileriGöster() {
-    console.log(
-      `isim: ${this.isim} soyisim: ${this.soyisim} maas: ${this.maas} yaş: ${this.yas}`
-    );
-  }
-}
-const insan1 = new Insan("baran", "şahin", 25, 35000);
-const insan2 = new Insan("sema", "aydin", 21, 20000);
-insan1.bilgileriGöster();
-insan2.bilgileriGöster();
+//   bilgileriGöster() {
+//     console.log(
+//       `isim: ${this.isim} soyisim: ${this.soyisim} maas: ${this.maas} yaş: ${this.yas}`
+//     );
+//   }
+// }
+// const insan1 = new Insan("baran", "şahin", 25, 35000);
+// const insan2 = new Insan("sema", "aydin", 21, 20000);
+// insan1.bilgileriGöster();
+// insan2.bilgileriGöster();
+
+// STATİC
+// class Matematik {
+//   topla(a, b) {
+//     console.log(a + b);
+//   }
+//   cikarma(a, b) {
+//     console.log(a - b);
+//   }
+//   static carp(a, b) {
+//     console.log(a * b);
+//   }
+// }
+// const matematik = new Matematik();
+// matematik.carp(5, 6); // static olmadıği için nesne üzerinden erişiyoruz
+// Matematik.carp(5, 6); // static olduğu için class üzerinden erişiyoruz
